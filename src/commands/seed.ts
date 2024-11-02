b19c8cdb6773978e4c95ea15f29c0fe10219af6a
gksx <68240027+devgksx@users.noreply.github.com.>
2024-11-02 17:32:58 -0400

    Seed command

---
+++ src/commands/seed.ts
@@ -0,0 +1,12 @@
+import { bot, Command } from "..";
+
+export const seedCommand : Command = {
+  command : "seed",
+  usage : "seed",
+  permission : 1,
+  desc : "World seed",
+  exec : async (username) => {
+    bot.chat(`/w ${username} 7540332306713543803 or copy it here https://pastebin.com/raw/rBPFKiNA`);
+    return true;
+  }
+}
