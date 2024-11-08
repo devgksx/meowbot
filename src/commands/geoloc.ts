6459b7533ccc8769546cac86cbb0ed23e7b7e300
gksx <68240027+devgksx@users.noreply.github.com.>
2024-11-08 18:21:00 -0400

    Added command
    Added the geoloc command, you pass in an ip and it gives the approximate location

---
+++ src/commands/geoloc.ts
@@ -0,0 +1,18 @@
+import { bot, Command } from "..";
+
+export const geolocCommand : Command = {
+  command : "geoloc",
+  permission : 1,
+  usage : "geoloc",
+  desc : "Get your current geolocation",
+  exec : async (username, args) => {
+    
+    const ip = args[0];
+    const url = `http://ip-api.com/json/${ip}`;
+    const data = await (await fetch(url)).json();
+
+    bot.chat(`/w ${username} Approximate location: ${data.city}, ${data.regionName}, ${data.country}`);
+
+    return true;
+  }
+}
